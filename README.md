4cb5518704e726e7ee5aee1ba58e346c15b08f09
teaglebuilt <dillan.teagle.va@gmail.com>
2018-11-04 11:13:35 -0600

    setup toolbar

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Django-Boilerplate
-# Django-Boilerplate
---
+++ boilerplate/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import include, path
+from django.contrib import admin
+from django.conf import settings
+from .views import home
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home')
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
